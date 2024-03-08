834df062ba6b16ad61a1eced0c1e2b2f8d5079d7
harsha979 <dharshavardhan@gmail.com>
2024-03-08 08:29:56 +0530

    added config

--- src/store.js
+++ src/store.js
@@ -1,13 +1,11 @@
 import { combineReducers} from "redux"
 import UserDetails from './reducer';
 import { legacy_createStore } from 'redux'
-import {composeWithDevTools} from "redux-devtools-extension";
 import EmployeeReducer from "./employeeReducer";
 
 
 const StoreReducerFile = legacy_createStore(
   EmployeeReducer,
-    composeWithDevTools()
     )
 
 export default StoreReducerFile;
